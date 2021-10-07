7765c28dfc5fedbc0626ebd7c7532c9693e316d9
hajer <h@MacBook-Pro-hajer.local>
2021-10-07 18:01:58 +0300

    complet homework

--- 07 Parameters and Results/ParametersAndResults.playground/Pages/Exercise-Verbing Your Noun.xcplaygroundpage/Contents.swift
+++ 07 Parameters and Results/ParametersAndResults.playground/Pages/Exercise-Verbing Your Noun.xcplaygroundpage/Contents.swift
@@ -8,9 +8,10 @@
  - callout(Exercise): Write a function that returns a sentence like “Row, row, row your boat” when given a verb and a noun argument. The function should look like this when you call it:\
  `let line = openingLine(verb: "Row", noun: "Boat")`
 */
-
-
-
+func openingLine(verb:String, noun:String)->String{
+    return "\(verb),\(verb),\(verb) your \(noun)."
+}
+openingLine(verb: "Row", noun: "Boat")
 
 
 
--- 07 Parameters and Results/ParametersAndResults.playground/Pages/Other Favorite Things.xcplaygroundpage/Contents.swift
+++ 07 Parameters and Results/ParametersAndResults.playground/Pages/Other Favorite Things.xcplaygroundpage/Contents.swift
@@ -12,7 +12,11 @@
  - callout(Exercise): Put the print statement above inside a function that allows you to pass in one string for the category of thing, and another string for your favorite. When you call the function, it should look like this:\
  `printFavorite(categoryOfThing: "food", favorite: "cheese")`
 */
-
+func myFavorite (categoryOfThing:String ,favorite:String){
+    print("My favorite \(categoryOfThing) is \(favorite)")
+    
+}
+myFavorite (categoryOfThing:"food" ,favorite:"cheese")
 
 //: You’ve learned how to pass information in to a function. Now learn how to get information out.
 //:
